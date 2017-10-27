af2e14c8e6cb952236dc16456d227c63ba306683
Marcelo Glezer <mg@tekii.com.ar>
2017-10-27 17:53:13 -0300

    not working dashboard

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -9,7 +9,21 @@ class Dashboard extends Component {
 
     render() {
         return (
-            <h1>-- Dashboard Here --</h1>
+            <div className="mt-20">
+                <div className="row">
+                    <div className="col-md">
+                        <h1>{val.name}</h1>
+                    </div>
+                    <div className="col-md">
+                        <h1>{val.name}</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md">
+                        <h1>{val.name}</h1>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
