0a14ab05e4239d5e79a559095cc637972c2ee4c7
Marcelo Glezer <mg@tekii.com.ar>
2017-10-27 17:12:02 -0300

    placeholder now respects size

--- src/card/placeholder.js
+++ src/card/placeholder.js
@@ -17,7 +17,7 @@ export default class FrontCard extends Component {
                         <h5 className="mt-0">Click to add</h5>
                         <h6>{this.props.companyRole}</h6>
                      </div>
-                     <img className="ml-3" src={require('../images/placeholder.png')} alt="User name"/>
+                     <img className="ml-3 contact-photo" src={require('../images/placeholder.png')} alt="User name"/>
                 </div>
                 <div className="mt-3 row user-info">
                     <div className="col-md text-left">
